a30164814bc34ccf874f49a74432b5df68e66ac9
Derek <derekwilliamsfl@gmail.com>
2019-04-20 09:07:40 -0400

    Updates

---
+++ precache-manifest.7a2d848fb322795b01f8a730137fb395.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "c44107bebcda80d6075b",
+    "url": "/Served-Soft-PWA/static/css/main.d115f74f.chunk.css"
+  },
+  {
+    "revision": "c44107bebcda80d6075b",
+    "url": "/Served-Soft-PWA/static/js/main.e7e6144c.chunk.js"
+  },
+  {
+    "revision": "a5b870ea82bab4153b9b",
+    "url": "/Served-Soft-PWA/static/js/runtime~main.78106dc2.js"
+  },
+  {
+    "revision": "f951894e7ddae101abeb",
+    "url": "/Served-Soft-PWA/static/css/2.e3fbb52d.chunk.css"
+  },
+  {
+    "revision": "f951894e7ddae101abeb",
+    "url": "/Served-Soft-PWA/static/js/2.b7ebe9ef.chunk.js"
+  },
+  {
+    "revision": "4528b3bd2e772542506dff2bc39cd816",
+    "url": "/Served-Soft-PWA/static/media/cheeseburger.4528b3bd.png"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Served-Soft-PWA/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Served-Soft-PWA/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Served-Soft-PWA/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Served-Soft-PWA/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "7a699f2bd36c22a01ebbfb7c6fd3410f",
+    "url": "/Served-Soft-PWA/static/media/logo.7a699f2b.png"
+  },
+  {
+    "revision": "461fecb5bfe0ea124de2e55aeaac5aa3",
+    "url": "/Served-Soft-PWA/static/media/jollibee.461fecb5.png"
+  },
+  {
+    "revision": "3f06a1f3410075a5cf8f6e4b82bcb98c",
+    "url": "/Served-Soft-PWA/static/media/ice-cream.3f06a1f3.webp"
+  },
+  {
+    "revision": "f17d093caa09ebbebe9ca9f9c655b669",
+    "url": "/Served-Soft-PWA/static/media/two_icecream.f17d093c.svg"
+  },
+  {
+    "revision": "b3face1cc39fe698d3cf40401f792548",
+    "url": "/Served-Soft-PWA/static/media/french fries.b3face1c.png"
+  },
+  {
+    "revision": "e6bea1a8a7fe80185907716fd6457b87",
+    "url": "/Served-Soft-PWA/static/media/strawberryicecream.e6bea1a8.png"
+  },
+  {
+    "revision": "d61a4b2c25d006229d98809ae0953f17",
+    "url": "/Served-Soft-PWA/static/media/pineapplemilkshake.d61a4b2c.png"
+  },
+  {
+    "revision": "408bdf4ab8297576b4e1b129dc0634e7",
+    "url": "/Served-Soft-PWA/static/media/raspberrywine.408bdf4a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Served-Soft-PWA/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8b27bc96115c2d24350f0d09e6a9433f",
+    "url": "/Served-Soft-PWA/static/media/fontawesome-webfont.8b27bc96.eot"
+  },
+  {
+    "revision": "3293616ec0c605c7c2db25829a0a509e",
+    "url": "/Served-Soft-PWA/static/media/fontawesome-webfont.3293616e.woff"
+  },
+  {
+    "revision": "dcb26c7239d850266941e80370e207c1",
+    "url": "/Served-Soft-PWA/static/media/fontawesome-webfont.dcb26c72.ttf"
+  },
+  {
+    "revision": "0a799148a50bb02c6f380eabd8d97559",
+    "url": "/Served-Soft-PWA/static/media/fontawesome-webfont.0a799148.svg"
+  },
+  {
+    "revision": "2b6dc9709b1c3501796489cb19eb167c",
+    "url": "/Served-Soft-PWA/static/media/two_friends.2b6dc970.jpg"
+  },
+  {
+    "revision": "8f37d8a4f0f278d85e5e4be87399cfe8",
+    "url": "/Served-Soft-PWA/index.html"
+  }
+];
